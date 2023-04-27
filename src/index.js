796ef7d9c0f66aa023cb574e416baba3b3401386
Itamar Zwi <itamarz@amplicy.io>
2023-04-27 16:26:57 +0300

    Crash example

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 const { createBullBoard } = require("@bull-board/api");
 const { BullAdapter } = require("@bull-board/api/bullAdapter");
-const { ExpressAdapter } = require("@bull-board/express");
 const Bull = require("bull");
-
-const serverAdapter = new ExpressAdapter();
+const { MyServerAdapter } = require("./my-server-adapter");
 
 const queue = new Bull();
 
 createBullBoard({
   queues: [new BullAdapter(queue)],
-  serverAdapter: serverAdapter,
+  serverAdapter: new MyServerAdapter(),
 });
 
 console.log("Hello world");
---
+++ src/my-server-adapter.js
@@ -0,0 +1,33 @@
+class MyServerAdapter {
+  constructor() {}
+
+  setQueues() {
+    return this;
+  }
+
+  setViewsPath() {
+    return this;
+  }
+
+  setStaticPath() {
+    return this;
+  }
+
+  setEntryRoute() {
+    return this;
+  }
+
+  setErrorHandler() {
+    return this;
+  }
+
+  setApiRoutes() {
+    return this;
+  }
+
+  setUIConfig() {
+    return this;
+  }
+}
+
+module.exports = { MyServerAdapter };
